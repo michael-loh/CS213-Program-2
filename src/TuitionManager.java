accd50dcd90066fc0cfc6ec898ef1c876643f0d3
varunv5000 <42180864+varunv5000@users.noreply.github.com>
2020-02-28 15:27:31 -0500

    Update TuitionManager.java

--- src/TuitionManager.java
+++ src/TuitionManager.java
@@ -1,12 +1,82 @@
+/**
+ * The TuitionManager class is the interface class which processes user commands.
+ * @author Varun Vasudevan
+ */
+import java.util.Scanner;
 
 public class TuitionManager {
+	Scanner stdin;
+	StudentList list;
 
-	public static void main(String[] args) {
-		run();
+
+	
+	private void run() {
+		stdin = new Scanner(System.in);
+		list = new StudentList();
+		boolean done = false;
+		
+	      
+		while ( !done )
+		{
+			String command = stdin.next();
+			
+			switch ( command )  
+			{   
+				case "I":  
+					inStateAdd();
+					break; 
+				case "O": 
+					outStateAdd();
+					break;
+				case "N": 
+					international();
+					break;
+				case "R": 
+					remove();
+					break;
+				case "P":
+					print();
+					break;
+				case "Q":
+					quit();
+					done = true;
+				default: 
+					System.out.println("Command '" + command + "' is not supported!");
+					stdin.nextLine();
+			}  
+		}
 	}
 	
-	private static void run() {
-		
+	private void inStateAdd() {
+		String fname = stdin.next();
+		String lname = stdin.next();
+		int credit = stdin.next();
+		int funds = stdin.next();
+		if (credit <1) {
+			invalidCredit();
+			return;
+		}
+		if (funds<0) {
+			invalidFunds();
+			return;
+		}
+		Instate s = new Instate(fname,lname,credit,funds);
+		 if (list.contains(s)){
+			 studentInListError();
+			 return;
+		 }
+		 list.add(s);
+	}
+	
+	private void invalidCredit() {
+		System.out.println("Invalid credit amount!");
+	}
+	private void invalidFunds() {
+		System.out.println("Invalid fund amount!");
+	}
+	private void studentInListError() {
+		System.out.println("Student is already in list");
 	}
 
 }
+
